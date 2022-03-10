1bb562a59ad5235d5004ebb7de7742b00c151356
Bunnet <phoung.bunnet19@kit.edu.kh>
2022-03-10 15:32:20 +0700

    update tt

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', (e) => {
   const data = e.data.json();
-  self.registration.showNotification(data.title, {
-    body:data.body,
-    icon:data.icon,
+  self.registration.showNotification("Hmm", {
+    body:data.text(),
+    icon:'https://cdn-icons-png.flaticon.com/512/7058/7058778.png',
   });
 });
